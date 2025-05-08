480a2b8294f63d1f75beee60a867eae257f33d03
quang <vinhquan001@e.ntu.edu.sg>
2025-05-08 23:58:25 +0700

    add postgres db port to auth

--- src/auth.ts
+++ src/auth.ts
@@ -12,6 +12,7 @@ const pool = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT, 10) : undefined,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
